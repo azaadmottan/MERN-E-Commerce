ff407c1828581d4672b0154c80623fd2e4593b2a
Azaad Mottan <azaadmottan2022@gmail.com>
2024-07-06 12:12:28 +0530

    Install the frontend library i.e vite + reactJs, and install the dependencies i.e tailwind CSS for designing, setup the folder structure create necessary folders to handle the frontend efficiently.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+    return (
+    <>
+    <div className="text-3xl flex items-center justify-center text-orange-500 font-bold leading-loose">
+        Welcome to ShopKart
+    </div>
+    </>
+    );
+}
+
+export default App;
