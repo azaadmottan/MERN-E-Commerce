d69efc2675cbfb3e06d947bcfce3d05ff6f5691c
Azaad Mottan <azaadmottan2022@gmail.com>
2024-07-15 23:45:31 +0530

    Setup the config file for backend urls for efficient communication b/w the frontend and backend.

---
+++ frontend/src/config/api.config.js
@@ -0,0 +1,16 @@
+// const API_BASE_URL = "http://localhost:8000/api/v1"
+const API_BASE_URL = "/api"
+
+export const API_URLS = {
+    USER: `${API_BASE_URL}/users`,
+    PRODUCTS: `${API_BASE_URL}/products`,
+    CART: `${API_BASE_URL}/cart`,
+    CATEGORIES: `${API_BASE_URL}/category`,
+    REVIEWS: `${API_BASE_URL}/reviews`,
+    ADDRESS: `${API_BASE_URL}/address`,
+    COUPON: `${API_BASE_URL}/coupon`,
+}
+
+export const PUBLIC_URL = {
+    PUBLIC_STATIC_URL: `http://localhost:8000`,
+}
