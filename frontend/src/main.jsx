6d41f5eb9edbf0ab516ed11d4f2ea5bb7e2e8029
Azaad Mottan <azaadmottan2022@gmail.com>
2024-07-15 23:50:28 +0530

    Setup of App with store and routes.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Provider } from "react-redux";
+import store from './store/store.js';
+import Routes from './routes/Routes.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    // <React.StrictMode>
+        <Provider store={store}>
+            <Routes />
+        </Provider>
+    // </React.StrictMode>,
 )
