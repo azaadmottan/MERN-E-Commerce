0851d830bea28a51408ccd6b7a1d6f1ebd22b14d
Azaad Mottan <azaadmottan2022@gmail.com>
2024-07-18 14:47:10 +0530

    Create breadCrumb for dashboard.

---
+++ frontend/src/components/BreadCrumb.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import {
+    FaAngleRight
+} from "./Icons.jsx";
+
+function Breadcrumb() {
+    const location = useLocation();
+    const pathnames = location.pathname.split('/').filter(x => x);
+
+    return (
+        <nav>
+            <ul className="flex items-center gap-2 text-blue-500 cursor-default">
+                {
+                    pathnames.map((value, index) => {
+                        const to = `/${pathnames.slice(0, index + 1).join('/')}`;
+                        return (
+                            <li key={to}>
+                                <span to={to} className="flex items-center gap-2 hover:text-red-500">
+                                    {value}
+                                    <FaAngleRight />
+                                </span>
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+        </nav>
+    );
+}
+
+export default Breadcrumb;
