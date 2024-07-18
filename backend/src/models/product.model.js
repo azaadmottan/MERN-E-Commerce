bdeca557cb9a5778f4fdf9bc3ab0c39878bf767d
Azaad Mottan <azaadmottan2022@gmail.com>
2024-07-18 14:42:26 +0530

    Add attribute field in the product model.

--- backend/src/models/product.model.js
+++ backend/src/models/product.model.js
@@ -61,6 +61,10 @@ const productSchema = new Schema({
         required: true, 
         default: 1 
     },
+    attributes: {
+        type: Schema.Types.Mixed,
+        default: {}
+    }
 },
 {timestamps: true});
 
