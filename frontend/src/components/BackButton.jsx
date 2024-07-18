0ded160675db414787404adde83099edf93ea38a
Azaad Mottan <azaadmottan2022@gmail.com>
2024-07-18 14:46:33 +0530

    Create backButton component for dashboard.

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import {
+    IoArrowBack,
+} from "./Icons.jsx";
+
+function BackButton() {
+    const navigate = useNavigate();
+
+    const navigateBack = () => {
+        navigate(-1);
+    }
+
+    return (
+    <>
+        <button 
+        onClick={() => navigateBack()}
+        className='bg-slate-50 px-4 py-3 text-2xl rounded-md hover:bg-slate-100 transition-all duration-100 delay-75'
+        >
+            <IoArrowBack />
+        </button>
+    </>
+    )
+}
+
+export default BackButton;
