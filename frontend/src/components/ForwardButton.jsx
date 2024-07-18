99349b7bdbeffed054c841c66a2010e87ab61a5e
Azaad Mottan <azaadmottan2022@gmail.com>
2024-07-18 14:46:57 +0530

    Create forwardButton for dashboard.

---
+++ frontend/src/components/ForwardButton.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import {
+    IoArrowForward,
+} from "./Icons.jsx";
+
+function ForwardButton() {
+    const navigate = useNavigate();
+
+    const navigateForward = () => {
+        navigate(+1);
+    }
+
+    return (
+    <>
+        <button 
+        onClick={() => navigateForward()}
+        className='bg-slate-50 px-4 py-3 text-2xl rounded-md hover:bg-slate-100 transition-all duration-100 delay-75'
+        >
+            <IoArrowForward />
+        </button>
+    </>
+    )
+}
+
+export default ForwardButton;
