ea0fd06003ff345b575513c9bc3e1925bd69941d
Azaad Mottan <azaadmottan2022@gmail.com>
2024-08-20 23:17:23 +0530

    Setup the user info page.

--- frontend/src/pages/admin/UserInfo.jsx
+++ frontend/src/pages/admin/UserInfo.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import moment from 'moment';
 import { toast } from "react-toastify";
-import { getUserById, updateAccountActivity } from '../../actions/requestProduct.actions.js';
+import { getUserById, updateAccountActivity, updateUserRole } from '../../actions/requestProduct.actions.js';
 import { PUBLIC_URL } from "../../config/api.config.js";
+import { Modal } from "../../components/index.jsx";
 
 function UserInfo() {
     const { id } = useParams();
@@ -33,7 +34,7 @@ function UserInfo() {
     const [disableBtn, setDisableBtn] = useState(false);
     const handleUpdateActivity = async () => {
         setDisableBtn(true);
-        if (!activity) {
+        if (!activity || (activity === "none")) {
             toast.error("Please select activity status");
             setDisableBtn(false);
             return;
@@ -49,6 +50,33 @@ function UserInfo() {
             toast.error("Something went wrong while updating activity status");
         }
         setDisableBtn(false);
+    }
+
+    const [role, setRole] = useState("");
+    const [roleBtn, setRoleBtn] = useState(false);
+    const handleUpdateUserRole = async() => {
+        setRoleBtn(true);
+        if (!role || (role === "none")) {
+            toast.error("Please select user role");
+            setRoleBtn(false);
+            return;
+        }
+
+        const response = await updateUserRole(id, role);
+        if (response?.success) {
+            toast.success("User role updated successfully");
+            fetchUserDetails();
+            setRole("");
+        }
+        if (response?.error) {
+            toast.error("Something went wrong while updating user role");
+        }
+        setRoleBtn(false);
+    }
+
+    const [showDeleteAccountModal, setShowDeleteAccountModal] = useState(false);
+    const handleDeleteAccount = () => {
+        setShowDeleteAccountModal(false);
     }
 
     return (
@@ -87,9 +115,9 @@ function UserInfo() {
                 </h3>
 
                 <div className="flex items-center gap-2 my-4 px-4 py-2 rounded-md border-2 border-orange-500 ">
-                    <div className="w-[30%] h-[200px] flex items-center">
+                    <div className="w-[30%] h-[60%] flex items-center rounded-md">
                         <img 
-                        className="w-full h-full object-contain"
+                        className="w-full h-full object-contain rounded-md"
                         loading='lazy'
                         src={PUBLIC_URL.PUBLIC_STATIC_URL + "/" + userDetail?.profilePicture} alt={userDetail?.userName} />
                     </div>
@@ -103,6 +131,7 @@ function UserInfo() {
                         <p>
                             Email-id: <span className="text-gray-900">{userDetail?.email}</span>
                         </p>
+                        
                         <p className="flex flex-col gap-2">
                             <span>
                                 Account Activity
@@ -111,13 +140,35 @@ function UserInfo() {
                                 <select
                                 onChange={(e) => handleChangeActivity(e.target.value)}
                                 value={activity}
-                                className="px-10 py-1 text-gray-900 rounded-md border-2 focus-within:border-blue-500 outline-none cursor-pointer">
+                                className="px-10 py-1 sm:w-[45%] text-gray-900 rounded-md border-2 focus-within:border-blue-500 outline-none cursor-pointer">
+                                    <option value="none">Select Account Activity</option>
                                     <option value="true">Active Account</option>
                                     <option value="false">Block Account</option>
                                 </select>
                                 <button
                                 onClick={() => handleUpdateActivity()}
                                 disabled={disableBtn}
+                                className="text-white bg-blue-600 hover:bg-blue-700 px-3.5 py-1 rounded-md"
+                                >Update</button>
+                            </span>
+                        </p>
+
+                        <p className="flex flex-col gap-2">
+                            <span>
+                                User Role
+                            </span>
+                            <span className="flex items-center justify-between">
+                                <select
+                                onChange={(e) => setRole(e.target.value)}
+                                value={role}
+                                className="px-10 py-1 sm:w-[45%] text-gray-900 rounded-md border-2 focus-within:border-blue-500 outline-none cursor-pointer">
+                                    <option value="none">Select User Role</option>
+                                    <option value="true">Admin</option>
+                                    <option value="false">User</option>
+                                </select>
+                                <button
+                                onClick={() => handleUpdateUserRole()}
+                                disabled={roleBtn}
                                 className="text-white bg-blue-600 hover:bg-blue-700 px-3.5 py-1 rounded-md"
                                 >Update</button>
                             </span>
@@ -130,6 +181,7 @@ function UserInfo() {
                                     </span>
                                     <span>
                                         <button
+                                        onClick={() => setShowDeleteAccountModal(true)}
                                         className="text-white bg-red-500 hover:bg-red-600 px-4 py-1 rounded-md"
                                         >
                                             Delete
@@ -143,6 +195,23 @@ function UserInfo() {
             </>
             )
         }
+
+        {/* delete user modal */}
+        <Modal isOpen={showDeleteAccountModal} title="Confirm Delete User Account" onClose={() => setShowDeleteAccountModal(false)}>
+            <div className="grid gap-2">
+                <p className="my-2 text-lg font-semibold">Are you sure you want to delete this account ?</p>
+                <p className="my-2 text-red-600 font-medium italic tracking-wider">
+                    NOTE: Deleting this account will remove all associated data. After that account be recovered !
+                </p>
+
+                <button
+                onClick={() => handleDeleteAccount()}
+                className="text-white p-2 bg-blue-600 hover:bg-blue-700 rounded-md"
+                >
+                    Confirm Delete
+                </button>
+            </div>
+        </Modal>
     </div>
     </>
     )
